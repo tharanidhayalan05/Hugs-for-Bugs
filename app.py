285b2b04dc84ccf96e241cfee9edf4bd035c3572
tharanidhayalan05 <tharanidhayalan05@gmail.com>
2026-03-29 11:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
-app=Flask(_name_)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from team 4,5,6"
+    return "Hello Tharani"
 app.run(host="0.0.0.0",port=5001)
